aa0d01a5024e35816542e8b969f6f7a98a301093
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-09-02 16:29:57 +0300

    Inıtial commit

---
+++ src/test/java/JpaHibernate/Burger/BurgerApplicationTests.java
@@ -0,0 +1,13 @@
+package JpaHibernate.Burger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
